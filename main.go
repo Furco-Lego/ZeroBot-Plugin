9cfc82599cfd8e46b1e9fae772ccdd934de5bed5
Furco-Lego <87301689+Furco-Lego@users.noreply.github.com>
2024-01-12 15:28:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -259,7 +259,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 1417543044)
-	// sus = append(sus, 87654321)
+	// sus = append(sus, 1417543044)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
